ecbfc8e9fd260587400b8af26db374c81695862f
nietiene <niyomugaboetiene53@gmail.com>
2025-07-30 16:57:47 -0700

    commit of my-portiolio proj

--- my-portfolio/src/pages/home.jsx
+++ my-portfolio/src/pages/home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa"
-import { Link } from 'react-scroll'
+// import { FaGithub, FaLinkedin } from "react-icons/fa"
+// import { Link } from 'react-scroll'
 
 const Home = () => {
     return (
-        <
+        <div className="bg-blue-500">
+            hello
+        </div>
     )
 }
+
+
+export default Home
