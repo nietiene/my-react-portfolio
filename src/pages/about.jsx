93020360c8fa558ba927ca66dfde98b1fd1044eb
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 09:58:41 -0700

    commi of my portfolio

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,1 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import aboutMe from "../assets/19362653.jpg"
+
+const About = () => {
+    return (
+        <section className="min-h-screen flex flex-col md:flex-row items-center justify-between px-6 md:px-20 py-12 bg-gradient-to-br from-white via-gray-100 to-gray-200 text-gray-800">
+            <motion.img
+               src=""
+            />
+        </section>
+    )
+} 
