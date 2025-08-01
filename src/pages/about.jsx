31ff032287935d0a8108f24d8961a88234f69dfd
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 10:05:07 -0700

    commi of my portfolio

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -11,8 +11,22 @@ const About = () => {
                alt="About me Illustration"
                className="w-full md:w-1/2 max-w-lg mb-10 md:mb-0"
                initial={{ x: -100, opacity: 0 }}
-
+               animate={{ x: 0, opacity: 1 }}
+               transition={{ duration: 1}}
             />
+
+            <motion.div
+              className="md:1/2 space-y-6"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1 }}
+            >
+                <h2 className="text-4xl font-bold text-cyan-700">About Me</h2>
+                <p className="text-lg loading-relaxed">
+                    I'm <span className="font-semibold text-cyan-600">Etiene Niyomugabo</span>
+                </p>
+
+            </motion.div>
         </section>
     )
 } 
