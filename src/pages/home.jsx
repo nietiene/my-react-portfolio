d7e294dfb20dcf862b919880d820a08b0b6b044d
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 08:19:03 -0700

    commi of my portfolio

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
+import { Link } from "react-scroll"
+
+const Home = () => {
+ 
+    return (
+        <section
+          name="home"
+          className="h-screen w-full bg-gradient-to-b from-black via-gray-900 to-gray-800 text-white flex items-center">
+
+
+        </section>
+    )
+}
