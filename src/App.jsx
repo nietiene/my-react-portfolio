7ed6bb7039a678edec03874d8283051345e9c29d
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 16:48:24 -0700

    commi of my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import Home from './pages/home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import About from './pages/about'
 import Skills from './pages/skills'
+import Projects from './pages/projects'
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>} />
-        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />} />
+        <Route path='/skills' element={<Skills />}/>
+        <Route path='/projects' element={<Projects />}/>
       </Routes>
     </Router>
   )
