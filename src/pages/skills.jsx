655a86f81e183ff3a560dffdcef5701c0184880a
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 13:55:03 -0700

    commi of my portfolio

---
+++ src/pages/skills.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { motion } from "framer-motion";
+import {
+    FaHtml5, FaCss3Alt, FaJs, FaReact, FaBootstrap,
+    FaNodeJs, FaPython, FaPhp, FaDatabase, FaGitAlt,
+    FaGitHub, FaFigma
+} from "react-icons/fa"
+import {
+  SiTailwindcss, SiExpress, SiFlask, SiDjango, SiMongoDb,
+  SiPostman, SiVerser, SiVisualstudiocode, SiSocketio, 
+} from "react-icons/si"
+
+import skillsIllustration from "../assets/skills.webp"
