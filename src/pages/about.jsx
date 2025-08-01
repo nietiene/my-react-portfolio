10b776c108df570dffeef4138599f0b6c6574600
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 09:59:27 -0700

    commi of my portfolio

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,9 @@ const About = () => {
     return (
         <section className="min-h-screen flex flex-col md:flex-row items-center justify-between px-6 md:px-20 py-12 bg-gradient-to-br from-white via-gray-100 to-gray-200 text-gray-800">
             <motion.img
-               src=""
+               src={aboutMe}
+               alt="About me Illustration"
+               className="w-full md:w-1/2 max-w-lg mb-10 md:mb-0"
             />
         </section>
     )
