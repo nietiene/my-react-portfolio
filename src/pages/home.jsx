3f2fef6ee1cf8cfd5be04c9cb7bb82dd04f1a5df
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 09:04:22 -0700

    commi of my portfolio

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa"
 import { Link } from "react-scroll"
 import { motion } from "framer-motion"
+import avataaars from "../assets/avataaars"
 
 const Home = () => {
  
@@ -11,7 +12,16 @@ const Home = () => {
         >
            <div className="max-w-6xl w-full flex flex-col md:flex-row items-center justify-between gap-12">
                 <motion.div
-                   className="flex items-center md:text-left"></motion.div>
+                   className="flex items-center md:text-left"
+                   initial={{ opacity: 0, x: -40 }}
+                   animate={{ opacity: 1, x: 0 }}
+                   transition={{ duration: 0.8}}
+                >
+                    <p className="text-lg text-cyan-400"> Welcome to my portfolio</p>
+                    <h1 className="text-4xl sm:text-6xl font-bold mt-2 leading-tight">
+                        Hi mI'm <span className="text-cyan-600">Etiene</span>
+                    </h1>
+                </motion.div>
             </div>
         </section>
     )
