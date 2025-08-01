2ab371f76c956746a8233acc23e91d8b2d1f7d30
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 09:33:55 -0700

    commi of my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
 import Home from './pages/home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-  <Home></Home>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
   )
 }
 
