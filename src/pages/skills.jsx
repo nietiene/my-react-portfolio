781432903d45136d1e746b033533aed3b5c3c771
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 13:57:56 -0700

    commi of my portfolio

--- src/pages/skills.jsx
+++ src/pages/skills.jsx
@@ -11,3 +11,19 @@ import {
 } from "react-icons/si"
 
 import skillsIllustration from "../assets/skills.webp"
+
+const skills = () => {
+    const skillsCategories = [
+        {
+            name: "Frontend",
+            items: [
+                { icon: <FaHtml5 className="text-orange-500"/>, name: "HTML5"},
+                { icon: <FaCss3Alt className="text-blue-500"/>, name: "CSS3"},
+                { icon: <FaJs className="text-yellow-400"/>, name: "JavaScript"},
+                { icon: <FaReact className="text-cyan-400"/>, name: "React"},
+                { icon: <FaBootstrap className="text-purple-500"/>, name: "Bootastrap"},
+                { icon: <FaBootstrap className="text-purple-500"/>, name: "Bootastrap"},
+            ]
+        }
+    ]
+}
