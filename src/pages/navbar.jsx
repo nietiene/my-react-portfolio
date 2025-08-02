4f4570716e7010e5f2ad6ca49bd2babfd5160fbc
nietiene <niyomugaboetiene53@gmail.com>
2025-08-02 07:39:31 -0700

    commi of my portfolio

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -4,4 +4,15 @@ import { motion } from "framer-motion"
 import {
   FaHome, FaUser, FaCode,
   FaProjectDiagram,
+} from "react-icons/fa"
+
+import  { Link } from "react-scroll"
+import avataaars from "../assets/avataaars.svg"
+
+const NavBar = () => {
+  const [activeAction, setAction] = useState("home")
+
+  const navItems = [
+    
+  ]
 }
