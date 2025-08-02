4c1146195869bc336219aa74e6ddf81aa193527f
nietiene <niyomugaboetiene53@gmail.com>
2025-08-02 08:14:37 -0700

    commi of my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,6 @@ function App() {
            <Route path='/about' element={<About />} />
            <Route path='/skills' element={<Skills />}/>
            <Route path='/projects' element={<Projects />}/>
-           <Route path='navbar' element={<Navbar />}/>
        </Routes>
       </main>
     </Router>
