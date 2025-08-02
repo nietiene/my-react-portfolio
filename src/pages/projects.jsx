401cb729fa1fba4e8fa7c3efd45f2febdf6e63aa
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 17:07:05 -0700

    commi of my portfolio

---
+++ src/pages/projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { FaGithub } from "react-icons/fa";
+import {
+    siReact,
+    SiNodedotjs,
+    SiMongoDb,
+    SiFirebase,
+    SiTailwindcss,
+    SiExpress,
+    SiTypescript,
+    SiNextdotjs
+} from "react-icons/si"
+
+import eccomerceIllustration from "../assets/ecomerce.png"
+import chatAppIllustration from "../assets/chat-app.png"
+import libraryAppIllustration from "../assets/library-managment.png"
+import myPortfolio from "../assets/portfolio.png"
