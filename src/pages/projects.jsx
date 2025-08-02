cfc38331b657f86207f293e4e49f398aa4dc2550
nietiene <niyomugaboetiene53@gmail.com>
2025-08-01 17:11:39 -0700

    commi of my portfolio

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -9,10 +9,27 @@ import {
     SiTailwindcss,
     SiExpress,
     SiTypescript,
-    SiNextdotjs
+    SiNextdotjs,
+    SiReact
 } from "react-icons/si"
 
 import eccomerceIllustration from "../assets/ecomerce.png"
 import chatAppIllustration from "../assets/chat-app.png"
 import libraryAppIllustration from "../assets/library-managment.png"
+
 import myPortfolio from "../assets/portfolio.png"
+
+const Projects = () => {
+    const projects = [
+        { 
+            title: "Real-Time Chat Application",
+            tech: [
+                { icon: <SiReact className="text-cyan-400" />, name: "React"},
+                { icon: <SiNodedotjs className="text-green-500" />, name: "Node.js"},
+                { icon: <SiFirebase className="text-yellow-400" />, name: "Firebase"},
+            ],
+            description: 
+            "A socket.io based"
+        }
+    ]
+}
