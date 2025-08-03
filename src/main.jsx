2789a0016b747d4c2e1402d38c560c39d3c09b56
nietiene <niyomugaboetiene53@gmail.com>
2025-08-02 17:35:34 -0700

    commi of my portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+const savedMode = localStorage.getItem('darkMode');
+const prefersDark = window.matchMedia('prefers-color-scheme: dark)').matches;
+const initialMode = savedMode !== null ? JSON.parse(savedMode) :  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
